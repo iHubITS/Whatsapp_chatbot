30ca3c70a3d9fab3202f10106d3fd616fca1416c
umbarjes28 <umbarjes28@gmail.com>
2020-11-09 16:04:58 +0530

    first commit

---
+++ chatbot_whatsapp_actions-master/actions.py
@@ -0,0 +1,59 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/core/actions/#custom-actions/
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+# from typing import Any, Text, Dict, List
+#
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
+from rasa_sdk.forms import FormAction
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from typing import Any, Text, Dict, List
+from rasa_sdk.events import SlotSet, Form
+import logging
+import datetime
+
+#logger = logging.getLogger(__name__)
+
+#REQUESTED_SLOT = "requested_slot"
+
+
+LANGUAGES = {
+    "telugu":
+        {
+            "name": "telugu",
+            "resource": "tl"
+        },
+    "english":
+        {
+            "name": "english",
+            "resource": "en"
+        },
+    "hindi":
+        {
+            "name": "hindi",
+            "resource": "hn"
+        }
+}
+        
+
---
+++ chatbot_whatsapp_actions-master/data/stories.md
@@ -0,0 +1,19 @@
+## happy path
+* greet
+  - find_language_types
+  - slot{"language":"telugu"}
+* goodbye
+  - utter_goodbye
+
+## happy path 1
+* goodbye
+  - utter_goodbye
+
+
+## say goodbye
+* goodbye
+  - utter_goodbye
+
+## bot challenge
+* bot_challenge
+  - utter_iamabot
